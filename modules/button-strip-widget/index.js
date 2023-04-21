5ba21783282b4787b871ff9d06d9469cf3f2864b
Darren Caboz <darren.caboz@hellocomputer.com>
2023-04-21 10:54:46 +0200

    adjustment to button strip for button fragment set up

--- modules/button-strip-widget/index.js
+++ modules/button-strip-widget/index.js
@@ -1,4 +1,4 @@
-const linkSchema = require('../../lib/linkSchema');
+const buttonSchema = require('../../lib/buttonSchema');
 
 module.exports = {
   extend: '@apostrophecms/widget-type',
@@ -13,65 +13,7 @@ module.exports = {
         label: 'Button strip',
         fields: {
           add: {
-            ...linkSchema.link,
-            style: {
-              type: 'select',
-              label: 'Color Style',
-              required: true,
-              choices: [
-                {
-                  label: 'Primary',
-                  value: 'primary'
-                },
-                {
-                  label: 'Secondary',
-                  value: 'secondary'
-                }
-              ]
-            },
-            size: {
-              type: 'select',
-              label: 'Size',
-              required: true,
-              choices: [
-                {
-                  label: 'Regular',
-                  value: ''
-                },
-                {
-                  label: 'Small',
-                  value: 'sm'
-                },
-                {
-                  label: 'Large',
-                  value: 'lg'
-                }
-              ]
-            }
-            // block: {
-            //   type: 'boolean',
-            //   label: 'Full Width',
-            //   def: false
-            // },
-            // alignment: {
-            //   type: 'select',
-            //   label: 'Button Alignment',
-            //   choices: [
-            //     {
-            //       label: 'Left',
-            //       value: 'left',
-            //       def: true
-            //     },
-            //     {
-            //       label: 'Center',
-            //       value: 'center'
-            //     },
-            //     {
-            //       label: 'Right',
-            //       value: 'right'
-            //     }
-            //   ]
-            // }
+            ...buttonSchema.button
           }
         }
       }
