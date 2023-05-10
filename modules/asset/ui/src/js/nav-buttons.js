d3ee9fcdd52664bbf9e092482ee748093ff49908
Yash <yashveer.sewparthab@hellocomputer.com>
2023-05-10 20:28:47 +0200

    navigation - design

--- modules/asset/ui/src/js/nav-buttons.js
+++ modules/asset/ui/src/js/nav-buttons.js
@@ -1,9 +1,14 @@
 export function navButton() {
   var navBtn = document.querySelector('.navigation__menu-btn');
-  
+  var navItems = document.querySelector('.navigation__nav-items');
+
 	if (navBtn && navBtn !== null) {
 	  navBtn.addEventListener('click', function () {
-		console.log('Hello World');
+		if(!navItems.classList.contains('navigation__show')) {
+			navItems.classList.add('navigation__show');			
+		} else {
+			navItems.classList.remove('navigation__show');
+		}
 	  });
 	}
 }
