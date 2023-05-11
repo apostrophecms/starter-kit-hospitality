d50d8213c5239fdfec08fa67de1d3128abe0afd9
Yash <yashveer.sewparthab@hellocomputer.com>
2023-05-11 15:25:32 +0200

    footer icons, nav and footer to cms

--- modules/asset/ui/src/js/nav-buttons.js
+++ modules/asset/ui/src/js/nav-buttons.js
@@ -1,15 +1,11 @@
 export function navButton() {
-  var navBtn = document.querySelector('.navigation__menu-btn');
-  var navItems = document.querySelector('.navigation__nav-items');
 
-	if (navBtn && navBtn !== null) {
-	  navBtn.addEventListener('click', function () {
-		if(!navItems.classList.contains('navigation__show')) {
-			navItems.classList.add('navigation__show');			
-		} else {
-			navItems.classList.remove('navigation__show');
-		}
-	  });
+	if (document.querySelector('.navigation__menu-btn')) {
+		const navBtn = document.querySelector('.navigation__menu-btn');
+		const navItems = document.querySelector('.navigation__nav-items');
+
+		navBtn.addEventListener('click', function () {
+			navItems.classList.toggle('navigation__show');
+		});
 	}
 }
-  
