1a2eaa0cc04618e946237bf240017f7e1c0f905f
Stuart Romanek <stuart@apostrophecms.com>
2023-07-28 11:11:32 -0400

    update readme and remove theme switching

--- modules/settings/index.js
+++ modules/settings/index.js
@@ -1,11 +1,9 @@
 const d = new Date();
 const year = d.getFullYear();
-const theme = process.env.THEME || 'hospitality';
 
 module.exports = {
   options: {
     ignoreNoCodeWarning: true,
-    year: year,
-    theme: theme
+    year: year
   }
 };
